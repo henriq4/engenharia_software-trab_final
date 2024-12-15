680c597bc014055173bfe0a5ead855c9faa450e1
Henrique GC <henriq4net@gmail.com>
2024-12-15 13:20:17 -0300

    feat: start building the frontend

---
+++ front/src/components/layout/data/sidebar-data.ts
@@ -0,0 +1,55 @@
+import {
+  IconBarrierBlock,
+  IconBrowserCheck,
+  IconBug,
+  IconChecklist,
+  IconError404,
+  IconHelp,
+  IconLayoutDashboard,
+  IconLock,
+  IconLockAccess,
+  IconMessages,
+  IconNotification,
+  IconPackages,
+  IconPalette,
+  IconServerOff,
+  IconSettings,
+  IconTool,
+  IconUserCog,
+  IconUserOff,
+  IconUsers,
+} from '@tabler/icons-react'
+import { AudioWaveform, Command, GalleryVerticalEnd } from 'lucide-react'
+import { type SidebarData } from '../types'
+
+export const sidebarData: SidebarData = {
+  user: {
+    name: 'Henrique GC',
+    email: 'henriquegc@gmail.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  teams: [
+    {
+      name: 'Biblioteca',
+      logo: Command,
+      plan: 'Bibliotecário',
+    },
+  ],
+  navGroups: [
+    {
+      title: 'General',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/',
+          icon: IconLayoutDashboard,
+        },
+        {
+          title: 'Estudantes',
+          url: '/students',
+          icon: IconUsers,
+        },
+      ],
+    },
+  ],
+}
